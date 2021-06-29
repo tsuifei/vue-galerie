37d3a8ac72ed00bea342b57fb1fe27a93f4d0dee
tsuifei <tsuifei@gmail.com>
2021-06-29 11:38:40 +0200

    [update] build files

--- dist/js/chunk-2d21de4f.46bf44b7.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21de4f"],{d2f1:function(t,n,o){"use strict";o.r(n);var c=o("7a23"),e={class:"product"},r=Object(c["h"])("h1",null,"Products 單頁面",-1);function u(t,n,o,u,d,s){return Object(c["t"])(),Object(c["d"])("div",e,[r])}var d={data:function(){return{product:{}}},created:function(){console.log(this.$route),console.log(this.$router)}};d.render=u;n["default"]=d}}]);
-//# sourceMappingURL=chunk-2d21de4f.46bf44b7.js.map
